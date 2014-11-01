492c0be540c35457b28cca5f4cd0474eb31265de
cravista <gcravista@gmail.com>
2014-11-01 19:10:26 -0200

    fixed delayorama python generator

--- fx/delayorama.py
+++ fx/delayorama.py
@@ -15,22 +15,19 @@ external = open("_delay~.pd", "w")
 external.write("#N canvas 375 123 450 300 10;")
 external.write("#X obj 0 0 inlet~;")
 external.write("#X obj 0 200 outlet~;")
-obj = 0
-
+obj = 2
 for n in range(0, options.refl):
   # cria delays
-  delwrite = "#X obj %d %d delwrite~ \$0-delay_%d %d;" % (n*160, 35, n, options.ms*(n+1))
-  delread = "#X obj %d %d delread~ \$0-delay_%d %d;" % (n*160, 100, n, options.ms*(n+1))
+  delwrite = "#X obj %d %d delwrite~ \$0-delay_%d %d;" % (n*160, 35, n, options.ms*(options.refl-n))
+  delread = "#X obj %d %d delread~ \$0-delay_%d %d;" % (n*160, 100, n, options.ms*(options.refl-n))
   if n == 0:
-    r = "#X obj %d %d *~ %d;" % (80, 70, options.decay/100)
+    r = "#X obj %d %d *~ %f;" % (80, 70, options.decay/100.0)
   else:
-    r = "#X obj %d %d *~ %d;" % (80+(n*140), 70, options.decay/100)
+    r = "#X obj %d %d *~ %f;" % (80+(n*140), 70, options.decay/100.0)
   external.write(delwrite)
-  obj = 1
   external.write(delread)
-  obj += 1
   external.write(r)
-  obj += 1
+  obj += 3
   
 
 c = "#X connect 0 0 2 0;\n"
@@ -44,11 +41,17 @@ external.write(c)
 #X connect 4 0 5 0;
 #X connect 6 0 7 0;
 #X connect 7 0 1 0;
-for n in range(0, obj):
+offset = 0
+for n in range(0, obj-1):
   c = "#X connect %d 0 %d 0;" % (n+2, n+3)
+  print "connecting %d ->  %d" % (n+2, n+3)
   external.write(c)
+  offset += 1
   #external.write(ccc)
 
-c = "#X connect %d 0 1 0;\n" % (obj)
+
+c = "#X connect %d 0 1 0;\n" % (obj-1)
+cc = "#X connect %d 0 2 0;\n" % (obj-1)
 external.write(c)
+external.write(cc)
 external.close()
